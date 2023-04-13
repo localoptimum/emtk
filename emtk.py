358c4d4283f2faa4a063bde7d5aac17959daed4f
Phil Bentley <phil.m.bentley@gmail.com>
2023-04-13 17:44:32 +0200

    Initial implementation of sharp Porod and fractal scattering.

--- emtk.py
+++ emtk.py
@@ -49,6 +49,8 @@ curv.plotFit(logarithmic=True) # display a graph of the fitted data
 
 import numpy as np
 
+import powerlaw
+
 import matplotlib.pyplot as plt
 
 from scipy.special import erf
@@ -617,7 +619,7 @@ class MLECurve:
         slic = (hi-lw)/(nbins+1)
 
         #print(self.data)
-        print(hi, lw, slic)
+        #print(hi, lw, slic)
         
         #if(logarithmic ==False):
         hbins = np.arange(lw, hi, slic)
@@ -1246,6 +1248,95 @@ class hardSphereCurve(MLECurve):
             
 
 
+
+
+
+
+
+
+class porodCurve(MLECurve):
+    def init(self, data):
+        self.nparams=2
+
+        if len(data) > 0:
+            self.setupGuesses()
+
+    def mle(self):
+        """Analytical MLE of gaussian distribution parameters, using 
+           powerlaw package.
+
+        """
+
+        self.method = "analytically"
+
+        results = powerlaw.Fit(self.data)
+        alpha = results.power_law.alpha
+        xmin = results.power_law.xmin
+        
+        self.estimates = np.array([alpha, xmin])
+    
+    def curve(self, params, dat=np.array(None)):
+        # Return the basic likelihood curve
+        alpha = params[0]
+        qmin = params[1]
+
+        if np.any(dat == None):
+            xx = self.data
+        else:
+            xx = dat
+            
+        #prd = (xx/qmin)**(-alpha) * (alpha - 1.0) / qmin
+        prd = (xx/qmin)**(-alpha) * (alpha - 1.0) / qmin
+        
+        return prd
+
+    def llcurve(self, params):
+        # Return the sum of the log likelihood for the curve shape
+        prd = self.curve(params)
+        # this stops warnings trying to do log(0.0)
+        lg = np.log(hrd, out=np.full_like(hrd, 1.0E-30), where= hrd!=0)
+        return np.sum(lg)
+
+    def cdf(self, params, xx):
+        alpha = params[0]
+        qmin = params[1]
+        
+        bkt = qmin / xx
+
+        pcdf = 1.0 - bkt**(alpha-1.0)
+        
+        return pcdf
+
+    def quantile(self, params, p):
+        alpha = params[0]
+        qmin = params[1]
+
+        qtl = qmin * (1.0 - p)**(-1.0 / (alpha-1.0))
+        
+        return qtl
+
+    
+    def setupGuesses(self):
+        self.guesses = np.array([4.0, 0.01])
+
+    
+    def report(self):
+        """Prints a brief report of the MLE fitting results.
+
+        """
+
+        print("Generalised Porod curve maximum likelihood estimation")
+        print(np.size(self.data), "data points")
+        print(self.guesses, "as initial guesses (z, qmin)")
+        print(self.estimates, "solution obtained", self.method)
+
+        #if self.verifyMaximum():
+        #    derivStr = "a maximum"
+        #else:
+        #    derivStr = "not a maximum"
+
+        #print("The second derivative indicates that this is", derivStr)
+        #print(self.uncertainty(), "uncertainty sigma (=root-variance)")
 
 
 
