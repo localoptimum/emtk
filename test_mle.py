c76efa0b78cf65011172cdda9e284662be359000
Phil Bentley <phil.m.bentley@gmail.com>
2023-04-20 11:01:28 +0200

    Refined the pytest script including CDF-Quantile check on all curves.

--- test_mle.py
+++ test_mle.py
@@ -45,11 +45,21 @@ def test_gauss():
     
 def test_lorentzian():
     loren = emtk.lorentzianCurve()
-
+    
     clength = 90.0 # correlation length in system
     kappa = 1.0 / clength
     params = np.array([kappa])
     xrange = np.array([0.001, 0.1])
+
+    # Test the inversion of CDF and Quantile functions
+    xx = 0.01
+    pp = loren.cdf(params, xx)
+    revx = loren.Quantile(params, pp)
+
+    assert xx == pytest.approx(revx)
+
+
+    # Create a test distribution and fit it
     loren.generateTestSamples(params, xrange, 2000)
     loren.mle()
 
@@ -57,12 +67,26 @@ def test_lorentzian():
     tolerances = np.full_like(diffs, 0.1)
     np.testing.assert_array_less(diffs, tolerances)
     
+
+
     
 def test_hardSpheres():
-    # This can be a little unstable sometimes...!
+    # This can be a little unstable sometimes...!  it has more samples
+    # than the lorentzian for that reason
     sph = emtk.hardSphereCurve()
     params = np.array([75.0])
     xrange = np.array([0.001, 0.1])
+
+
+    # Test the inversion of CDF and Quantile functions
+    xx = 0.01
+    pp = sph.cdf(params, xx)
+    revx = sph.Quantile(params, pp)
+
+    assert xx == pytest.approx(revx)
+
+
+    # Create a test distribution and fit it
     sph.generateTestSamples(params, xrange, 4000)
     sph.guesses = np.array([80.0])
     sph.mle()
@@ -73,10 +97,22 @@ def test_hardSpheres():
 
     
 def test_porod():
+    # This one can also be occasionally unstable I increased the
+    # number of samples to quite a bit more than the other curves
     curv = emtk.porodCurve()
     pvalues = np.array([4.0, 0.001])
     xrange = np.array([0.001, 0.01])
-    curv.generateTestSamples(pvalues, xrange, 4000)
+
+    # Test the inversion of CDF and Quantile functions
+    xx = 0.01
+    pp = curv.cdf(pvalues, xx)
+    revx = curv.Quantile(pvalues, pp)
+
+    assert xx == pytest.approx(revx)
+    
+    # Create a test distribution and fit it
+    
+    curv.generateTestSamples(pvalues, xrange, 6000)
     curv.guesses = np.array([3.6, 0.005])
     curv.mle()
     
