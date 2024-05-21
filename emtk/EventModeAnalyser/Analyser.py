15261713f46c95c7cc181e45afbc106d6baf5268
Phil Bentley <phil.m.bentley@gmail.com>
2024-05-21 14:45:18 +0200

    Added documentation to object code

--- emtk/EventModeAnalyser/Analyser.py
+++ emtk/EventModeAnalyser/Analyser.py
@@ -1,6 +1,6 @@
 """The EventModeAnalyser object is the main API for EMTK for users to perform event mode data analysis.
 
-import emtk.EventModeAnalyser as ema
+import emtk.EventModeAnalyser.EMAnalyser as ema
 """
 
 import copy
@@ -14,9 +14,21 @@ from scipy.stats import gaussian_kde
 
 import emcee
 
-from typing import Tuple
+from typing import Tuple # we are going to return tuples from functions, need that
 
-class Analyser:
+# To put in type hinting of returning a class object from a class member function you need to
+# persuade python to not suck.
+# This fiddle is constantly being tweaked by the python gods and breaks from sub-release to sub-release.
+# And lo... we should do:
+import sys
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+# Thanks yuanzz on stackoverflow 
+
+
+class EMAnalyser:
     """Main object with which users will interact.
 
     """
@@ -53,9 +65,9 @@ class Analyser:
         self.n_events = self.data.size
 
         if self.weights is None:
-            print("Analyser object created with", self.n_events, "events in range", self.xmin, "-", self.xmax)
+            print("EMAnalyser object created with", self.n_events, "events in range", self.xmin, "-", self.xmax)
         else:
-            print("Analyser object created with", self.n_events, "weighted events in range", self.xmin, "-", self.xmax)
+            print("EMAnalyser object created with", self.n_events, "weighted events in range", self.xmin, "-", self.xmax)
 
 
 
@@ -65,8 +77,8 @@ class Analyser:
             """The analyser object is created with a numpy array of events
 (x-values) and their weights:\n
 
-            import emtk.EventModeAnalyser.Analyser as ema
-            ema1 = ema.Analyser(events, weights)
+            import emtk.EventModeAnalyser.EMAnalyser as ema
+            ema1 = ema.EMAnalyser(events, weights)
 
 There are two provided workflows with the same API.  The first API
 does regular histogram and least squares fit analysis with lmfit as a
@@ -98,7 +110,8 @@ to only 50,000 for testing:
 You can then visualise the data using either a histogram or a kernel
 density plot: 
 
-            ema2.plot_histogram() ema2.plot_kde()
+            ema2.plot_histogram() 
+            ema2.plot_kde()
 
 If necessary you can specify the y-axis limits for your KDE plot:
 
@@ -178,11 +191,16 @@ Get the parameters and sigmas as determined by MCMC:
         https://en.m.wikipedia.org/wiki/Ternary_plot
         """
 
+        # Force the input to be a numpy array
         Qraw = np.asarray(Qraw)
-    
+
+        # TODO: Error checking for nonsense input...
+
+        # The final term is one minus the sum
         Qsum = np.sum(Qraw)    
         Qlast = 1.0 - Qsum
-        
+
+        # Add the final element to the array
         Qvals = np.append(Qraw, Qlast)
                 
         return Qvals
@@ -199,15 +217,23 @@ Get the parameters and sigmas as determined by MCMC:
 
         """
 
+        # Protect against calling when there are no data points
         if self.data is None:
             raise ValueError(
                 f"attempt to find optimal number of data points with no data defined."
                 )
 
+        # Apply the Freedman-Diaconis calculation
+        # First calculate the interquartile range of the data
         iqr = np.subtract(*np.percentile(self.data, [75, 25]))
+
+        # If all the data points are equal (or maybe there is only one data point)
+        # then the IQR is zero and that makes no sense for anything that comes after
         if iqr == 0.0:
             print("WARNING: interquartile range is zero.")
             return 0
+
+        # If we get to this point it's probably OK, return the Freedman-Diaconis value
         return int((self.xmax - self.xmin)*self.n_events**(1.0/3.0)/(2.0*iqr))
 
     
@@ -218,6 +244,8 @@ Get the parameters and sigmas as determined by MCMC:
         actual plotting is done by plot_histogram().
 
         """
+
+        # Protect against no data points
         if self.data is None:
             raise ValueError(
                 f"attempt to compute histogram with no data defined."
@@ -246,11 +274,19 @@ Get the parameters and sigmas as determined by MCMC:
         # created numpy.histogram object.
         self.histo = hst
 
+        # The way that numpy makes histograms is not x-y pairs but x bins
+        # We'll remove the last point and plot the histogram as a matplotlib step
+        # later with the step at the beginning of the point so it is correct.
         x_hist = hst[1]
         x_hist = x_hist[:-1]
+
+        # Grab the y values
         y_hist = hst[0]
+
+        # Error values are square root of y values (poisson statistics)
         e_hist = np.sqrt(y_hist)
 
+        # Save the results in the class variables for later use
         self.histx = x_hist
         self.histy = y_hist
         self.histe = e_hist
@@ -264,6 +300,8 @@ Get the parameters and sigmas as determined by MCMC:
         Setting log=True makes the y-axis logarithmic.
         Setting loglog=True makes both y- and x-axes logarithmic.
         """
+
+        # First we re-calculate the histogram data
         self.calculate_histogram()
 
         # Force the shape of the plot to be close to scipp for convenient comparisons.
@@ -342,11 +380,17 @@ Get the parameters and sigmas as determined by MCMC:
         slic=(self.xmax - self.xmin)/(nx+1)
         xgrid = np.arange(self.xmin, self.xmax, slic)
 
-        
+        # Call scipy's gaussian_kde method.
+        # In testing I find that a bandwidth of
+        # 20x the histogram bin width looks right
+
+        # TODO: a parameterisation of the bandwidth method
         kde = gaussian_kde(self.data, bw_method=20*slic, weights=self.weights)
-        xgrid_reshape = xgrid.reshape(-1, 1)
+        # xgrid_reshape = xgrid.reshape(-1, 1) # scikit learn again
         kde_line = kde.evaluate(xgrid)
 
+        # Now we need to normalise the curve correctly.
+        # We need a dx value for each data point, so we'll add one at the end
         end_delta = xgrid[-1] - xgrid[-2]
         fakepoint = xgrid[-1] + end_delta
         x_extra = np.append(xgrid, fakepoint)
@@ -354,10 +398,11 @@ Get the parameters and sigmas as determined by MCMC:
 
         dx = xshift - xgrid
 
+        # Now compute a numerical integral of the KDE curve
         slices = kde_line * dx
-
         integral = np.sum(slices)
 
+        # ... and store the normalised KDE curve in class variables for later use
         self.kdex = xgrid
         self.kdey = kde_line / integral
         self.kde = kde
@@ -372,26 +417,37 @@ Get the parameters and sigmas as determined by MCMC:
         Setting loglog=True plots the y- and x-axes on a log scale.
         
         """
-
+        # Force the limits to be a numpy array
         yr = np.asarray(ylimits)
 
+        # Re-calculate the KDE data
         self.calculate_kde()
 
+        # Create matplotplib objects
         fig,ax = plt.subplots()
-        
+
+        # Force a figure scale similar to scipp
         plt.rcParams["figure.figsize"] = (5.75,3.5)
 
+        # Plot the KDE
         plt.plot(self.kdex, self.kdey, label='Optimal KDE')
+
+        # If necessary, apply logarithmic axes
         if log or loglog:
             plt.yscale('log')
         if loglog:
             plt.xscale('log')
+
+        # Label axes and trim off the fat on the figure
         plt.ylabel('Intensity')
         plt.xlabel('Q (Å$^{-1}$)')
         plt.tight_layout()
 
-        if not yr.any() is None:
+        # Apply manual limits if specified
+        if not yr.any() is None and yr.size == 2:
             ax.set_ylim(yr)
+
+        # Add legend and show figure
         plt.legend()
         plt.show()
         
@@ -405,6 +461,8 @@ Get the parameters and sigmas as determined by MCMC:
         fit usually results in a single black line for the fit.  A poor
         fit that is not well converged will show many gray lines.
         """
+
+        
 
         flat_samples = self.sampler.get_chain(discard=100, thin=15, flat=True)
 
@@ -642,31 +700,36 @@ Get the parameters and sigmas as determined by MCMC:
 
     
 
-    def subsample(self, subsample_size: int) -> Analyser:
+    def subsample(self, subsample_size: int) -> Self:
         # Randomly subsamples the events to a smaller data size.
         # Returns a deep copy of the object.
 
+        # Make a deep copy of the self object
         copyobj = copy.deepcopy(self)
 
+        # Choose a random subset of the data elements
         rng = np.random.default_rng()
         elements = rng.choice(copyobj.n_events, subsample_size)
 
+        # get a local copy of those data elements
         subsample = copyobj.data[elements]
         subweights = copyobj.weights[elements]
 
+        # Write them over to the copy object
         copyobj.data = subsample
         copyobj.weights = subweights
         copyobj.n_events = subsample_size
 
-
-        
+        # Overwite the copy object data range
         copyobj.xmin = np.amin(copyobj.data)
         copyobj.xmax = np.amax(copyobj.data)
 
+        # reset the copy object class variables
         copyobj.lse_result = None
         copyobj.histo = None
         copyobj.kde = None
 
+        # return the copy object
         return(copyobj)
         
 
@@ -687,37 +750,47 @@ Get the parameters and sigmas as determined by MCMC:
         parameters.
 
         """
-        
+
+        # Force the parameter seed values to be a numpy array
         p0 = np.asarray(self.theta_seed)
+
+        # Protect against badly defined seed values
         self.ndim = p0.size
-        
         if p0.any() is None:
             raise ValueError(
                 f"attempt to launch MCMC with undefined initial theta_seed parameter values.  Define that first."
                 )
 
+        # Protect against an undefined log prior function
         if self.lpf is None:
             raise ValueError(
                 f"attempt to launch MCMC with undefined log prior function (llf).  Define that first."
                 )
 
+        # Protect against an undefined log likelihood function.
         if self.llf is None:
             raise ValueError(
                 f"attempt to launch MCMC with undefined log likelihood function (llf).  Define that first."
                 )
 
+        # If we get here, we will attempt to launch an MCMC analysis.
         print("MCMC launch")
 
 
-
+        # Create a spread of randomised starting points for each walker, that is
+        # clustered around the seed position
         p0 = [p0 + 1e-5 * np.random.randn(self.ndim) for k in range(self.nwalkers)]
 
+        # Create local copies of the class variables we need to pass to MCMC
+        # MCMC cannot see inside objects
         myllf = self.llf
         nwk = self.nwalkers
         ndm = self.ndim
 
+        # If the weights array is badly defined or not defined, use unity weights.
         if self.weights.any() is None:
             self.weights = np.ones_like(self.data)
+        # otherwise we assume the weights supplied by the user are correct and just proceed.
         
         # Set up the sampler.
         self.sampler = emcee.EnsembleSampler(nwk, ndm, myllf, args=[self.data, self.xmin, self.xmax, self.weights, self.lpf])
@@ -739,18 +812,23 @@ Get the parameters and sigmas as determined by MCMC:
         determined by MCMC.  The results are returned as a tuple.
 
         """
-        
+
+        # Grab a flat markov chain from the sampler
         samples=self.sampler.get_chain(flat=True)
 
+        # initialise the result arrays
         self.mcmc_parameter_values = np.zeros(self.ndim)
         self.mcmc_parameter_sigmas = np.zeros(self.ndim)
 
+        # Figure out how many parameters there are
         rge = range(self.ndim)
-        
+
+        # For each parameter, get the mean and standar deviation from the markov chain samples
         for i in rge:
             self.mcmc_parameter_values[i] = np.mean(samples[:,i])
             self.mcmc_parameter_sigmas[i] = np.std(samples[:,i])
 
+        # Return the means and standard deviations as a tuple
         return self.mcmc_parameter_values, self.mcmc_parameter_sigmas
         
 
@@ -772,67 +850,89 @@ Get the parameters and sigmas as determined by MCMC:
 
         """
 
+        # Get a flat chain 
         samps = self.sampler.get_chain(flat=True)
 
+        # Figure out how many parameters there are
         npars = samps.shape[1]
 
+        # Protect against bad user input for which parameter to investigate.
         if item < 0 or item >= npars:
             raise ValueError(
                 f"attempt to analyse a parameter with an index that is out of the range of the number of available parameters."
                 )
-        
+
+        # Get the mean and standard deviation of the markov chain samples
         p_mean = np.mean(samps[:,item])
         p_stddev = np.std(samps[:,item])
-
+    
+        # calculate the size graphical error bar to put on the plot
         barmin = p_mean - p_stddev
         barmax = p_mean + p_stddev
 
-
+        # We might also like to compare against least squares, so lets get those results
         lsp = np.asarray(self.least_squares_parameters)
         lsee = self.get_lse_param_sigmas()
 
+        # if there are no valid least squares results, or the user does not want a comparison,
+        # then our results are simple
         if lsp.any() is None or lsp.size < 1 or compare==False:
+            # we dont' do the comparison
             refLSE = False
+            # Create text labels based on the parameters
             pnam= "parameter [" + str(item) + "]"
             xlab = pnam
             ylab = "p(" + xlab + ")"
         else:
+            # there are valid least squares results and the user wants to have a comparison
             refLSE = True
+            # get the parameter names
             pnams = self.get_lse_param_names()
+            # remove the "amplitude" parameter because that does not apply for this way of
+            # doing MCMC on the x-axis
             pnams = pnams[1:]
+            # extract the relevant element from the name, value, and sigma
             pnam = pnams[item]
             lse_pval = lsp[item]
             lse_eval = lsee[item]
+            # create a label texts from that info 
             lstxt   = "LSE value " + str(round(lse_pval,4))
             xlab = pnam
             ylab = "p(" + xlab + ")"
 
+        # Create a text label from the MCMC result
         fittxt = "MCMC value " + str(round(p_mean,4))
 
+        # Make a histogram of the data points
         hst=plt.hist(samps[:,item], bins='auto', color='k', histtype="step")
+        # figure out the maximum y value of that histogram
         ytop = np.amax(hst[0])
+        # Label the axes accordingly with the previously established texts
         plt.xlabel(xlab)
         plt.ylabel(ylab)
-        #plt.vlines(true_kappa, 0, ytop*0.8, color='g')            
+        # The centre point and standard deviation are overlayed as a cross
         plt.vlines(p_mean, 0, ytop, color="red")
         plt.hlines(y=ytop*0.5, xmin=barmin, xmax=barmax, color="red")
-        #plt.xlim([true_kappa*0.7, true_kappa*1.3])
-        #plt.text(true_kappa*1.005, ytop*0.77, truetxt, color="g")
-        
+
+        # The parameter value is appended at the top of the centre bar
         plt.text(p_mean, ytop*0.97, fittxt, color="red")
 
+        
         if refLSE:
+            # if the comparison with LSE is done, we also make a blue cross marking the estimate
+            # centre value, standard deviation, and text label at the top
             plt.vlines(lse_pval, 0, ytop*0.9, ls='--', color='b')
             plt.hlines(y=ytop*0.9*0.5, xmin=lse_pval-lse_eval, xmax=lse_pval+lse_eval, ls='--', color='b')
             plt.text(lse_pval*1.005, ytop*0.87, lstxt, color="b")
 
+        # Apply logarithmic axis scaling if required:
         if log or loglog:
             plt.xscale('log')
 
         if loglog:
             plt.yscale('log')
 
-            
+        # Show the plot
         plt.show()
 
 
